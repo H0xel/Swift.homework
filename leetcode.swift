97adeea85cae8374a2ec940941e1645a8afc1a16
Ivan Amakhin <ivamakhin@gmail.com>
2021-04-10 14:57:06 +0300

    4th task

--- leetcode.swift
+++ leetcode.swift
@@ -48,3 +48,20 @@ func array(array: [Int], target: Int) -> [Int] {
         }
     return emptyArray
 }
+
+
+// Leetcode 4 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+var int = 32
+
+class Solution {
+        func reverse(_ x: Int) -> Int {
+            var x = x
+            var reversedX = 0
+            while x != 0 {
+                reversedX = reversedX * 10 + x % 10
+                x /= 10
+            }
+        return reversedX
+    }
+}
