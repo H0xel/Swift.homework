d03ab1e7375f9283809a584d715660dbbad51274
BeetLab <nikita.a.semenov@gmail.com>
2021-05-23 13:46:39 +0300

    errors handling

--- bank/bank/ProductStorage/ProductStorage.swift
+++ bank/bank/ProductStorage/ProductStorage.swift
@@ -46,3 +46,28 @@ class ProductStorageImpl: ProductStorage {
         }
     }
 }
+
+
+enum CustomError: Error {
+    case evenNumber
+    case error2(Error)
+    case error3
+}
+
+func someFunc(arg: Int) throws -> Int  {
+    if arg % 2 == 0 {
+        throw CustomError.evenNumber
+    } else {
+        return arg
+    }
+}
+
+func someFunc1(arg: Int) -> Int? {
+    if arg % 2 == 0 {
+        return nil
+    } else {
+        return arg
+    }
+}
+
+
